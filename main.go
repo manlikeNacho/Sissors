a587b1ea4b61969d657ef1069b212069f0001e69
Iheanacho Ebere <eiheanacho52@gmail.com>
2023-06-12 17:07:50 +0100

    Implemented Redis database config

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/manlikeNacho/Sissors/src/controller"
+	"github.com/manlikeNacho/Sissors/src/repository/sliceRepo"
+	"log"
+)
 
+func main() {
+	//Inittailize db
+	client := sliceRepo.New()
+	ctrl := controller.New(client)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to my url shortener",
+		})
+	})
+	r.POST("/Url", ctrl.CreateShortUrl)
+	if err := r.Run(":8080"); err != nil {
+		log.Printf("Server crashed due to, %v", err)
+	}
 }
--- src/repository/repository.go
+++ src/repository/repository.go
@@ -4,5 +4,5 @@ import "github.com/manlikeNacho/Sissors/src/models"
 
 type Repository interface {
 	SaveUrl(u models.Url) (string, error)
-	deleteUrl(u models.Url) error
+	DeleteUrl(u models.Url) error
 }
