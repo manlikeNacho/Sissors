25a334c58c1e1de17d766e0403c437952421c4b8
Iheanacho Ebere <eiheanacho52@gmail.com>
2023-06-16 22:12:13 +0100

    Feat

--- src/controller/controller_test.go
+++ src/controller/controller_test.go
@@ -33,3 +33,27 @@ func TestController_CreateShortUrl(t *testing.T) {
 	router.ServeHTTP(rr, request)
 	assert.NotNil(t, rr.Body)
 }
+
+func TestController_GetUrl(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	controllers := gomock.NewController(t)
+	defer controllers.Finish()
+	mockRepo := mocks.NewMockRepository(controllers)
+	ctrl := New(mockRepo)
+	testUrl := &models.Url{
+		ID:       "sum",
+		Url:      "google.com",
+		ShortUrl: "google",
+	}
+	mockRepo.EXPECT().SaveUrl(testUrl).Return(nil).AnyTimes()
+	mockRepo.EXPECT().GetUrl(testUrl.ShortUrl).Return(testUrl.Url, nil).AnyTimes()
+	router.POST("/Url", ctrl.CreateShortUrl)
+	rr := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodPost, "/Url", nil)
+	assert.NoError(t, err)
+
+	request.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(rr, request)
+	assert.NotNil(t, rr.Body)
+}
